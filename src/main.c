913a9ff68617c67d25e0ff2039317e7edd37c1da
Lucas Ramage <ramage.lucas@protonmail.com>
2021-05-17 06:39:21 -0400

    Block all syscalls
    Test with fork()
    
    Returns 12

--- src/main.c
+++ src/main.c
@@ -1,15 +1,19 @@
 #include <errno.h>
 #include <seccomp.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     scmp_filter_ctx ctx = NULL;
+    int pid;
 
-    ctx = seccomp_init(SCMP_ACT_ALLOW);
+    ctx = seccomp_init(SCMP_ACT_KILL_PROCESS);
 
     if (ctx == NULL) {
 	return ENOMEM;
     }
+
+    pid = fork();
 
     seccomp_release(ctx);
 }
